fa8dc4d07fb88141ca81943b24fbd996f4f33c42
pranjal singh <singhpranjal2312@gmail.com>
2025-09-18 19:17:22 +0530

    addition of database

--- app/database/db.py
+++ app/database/db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config_loader import settings
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://<user>:<password>@127.0.0.1:7400/calorie_counter'
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
